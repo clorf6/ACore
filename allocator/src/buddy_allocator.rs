049674a8ca6d35305c463bef8826c9dc23a2f603
clorf6 <2628474577@qq.com>
2024-06-16 00:59:39 +0800

    🦄 refactor(allocator): Remake a buddy allocator using binary tree.

--- allocator/src/buddy_allocator.rs
+++ allocator/src/buddy_allocator.rs
@@ -1,33 +1,38 @@
-use super::linked_list::LinkedList;
 use alloc::alloc::Layout;
 use core::alloc::GlobalAlloc;
+use core::cell::RefMut;
 use core::cmp::{max, min};
 use core::mem::size_of;
 use core::ptr::null_mut;
-use spin::Mutex;
+use sync::UPSafeCell;
+use log::debug;
 
-const BUDDY_ALLOCATOR_LEVEL: usize = 32;
-pub struct BuddyAllocator {
-    pub allocator: Mutex<BuddyAllocatorInner>,
+
+pub struct BuddyAllocator<const num: usize, const minimum: usize> {
+    pub allocator: UPSafeCell<BuddyAllocatorInner<num, minimum>>,
 }
 
-impl BuddyAllocator {
-    pub const fn new(minimum: usize) -> Self {
+impl<const num: usize, const minimum: usize> BuddyAllocator<num, minimum> {
+    pub const fn new() -> Self {
         Self {
-            allocator: Mutex::new(BuddyAllocatorInner::new(minimum)),
+            allocator: UPSafeCell::new(BuddyAllocatorInner::new()),
         }
     }
 
-    pub fn lock(&self) -> spin::MutexGuard<BuddyAllocatorInner> {
+    pub fn lock(&self) -> RefMut<'_, BuddyAllocatorInner<num, minimum>> {
         self.allocator.lock()
     }
 
-    pub unsafe fn add(&self, begin: usize, end: usize) {
-        self.allocator.lock().add(begin, end);
+    pub unsafe fn init(&self, offset: usize) {
+        self.allocator.lock().init(offset);
+    }
+
+    pub fn used(&self) -> usize {
+        self.allocator.lock().used
     }
 }
 
-unsafe impl GlobalAlloc for BuddyAllocator {
+unsafe impl<const num: usize, const minimum: usize> GlobalAlloc for BuddyAllocator<num, minimum> {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         self.lock().alloc(layout)
     }
@@ -37,100 +42,110 @@ unsafe impl GlobalAlloc for BuddyAllocator {
     }
 }
 
-pub struct BuddyAllocatorInner {
-    free_lists: [LinkedList; BUDDY_ALLOCATOR_LEVEL],
+pub struct BuddyAllocatorInner<const num: usize, const minimum: usize> {
     total: usize,
-    minimum: usize,
-    used: usize,
-    need: usize,
+    offset: usize,
+    pub used: usize,
+    block: [usize; num]
 }
 
-impl BuddyAllocatorInner {
-    pub const fn new(minimum: usize) -> Self {
+impl<const num: usize, const minimum: usize> BuddyAllocatorInner<num, minimum> {
+    pub const fn new() -> Self {
+        assert!(num.is_power_of_two());
+        assert!(minimum.is_power_of_two());
         Self {
-            free_lists: [LinkedList::new(); BUDDY_ALLOCATOR_LEVEL],
-            total: 0,
-            minimum: if minimum < size_of::<usize>() {
-                size_of::<usize>()
-            } else {
-                minimum
-            },
+            total: num * (minimum >> 1),
+            offset: 0,
             used: 0,
-            need: 0,
+            block: [0; num],
         }
     }
 
-    pub fn add(&mut self, mut begin: usize, mut end: usize) {
-        begin = (begin + self.minimum - 1) & (!self.minimum + 1);
-        end = end & (!self.minimum + 1);
-        let mut size = end - begin;
-        self.total += size;
-        while size > 0 {
-            let level = size.trailing_zeros() as usize;
-            unsafe {
-                self.free_lists[level].push_front(begin);
+    pub fn init(&mut self, offset: usize) {
+        self.offset = offset;
+        let mut node_size = self.total << 1;
+        for i in 0..num - 1 {
+            if (i + 1).is_power_of_two() {
+                node_size >>= 1;
             }
-            size -= 1 << level;
-            begin += 1 << level;
-        }
-    }
-
-    fn split(&mut self, big: usize, small: usize) {
-        for i in (small..big).rev() {
-            let now = self.free_lists[i + 1]
-                .pop_front()
-                .expect("[buddy allocator] Free list is empty.");
-            unsafe {
-                self.free_lists[i].push_front(now as usize + (1 << i));
-                self.free_lists[i].push_front(now as usize);
-            }
-        }
-    }
-
-    fn merge(&mut self, begin: usize, ptr: *mut u8) {
-        let mut now = ptr as usize;
-        for i in begin..self.free_lists.len() {
-            let buddy = now ^ (1 << i);
-            let node = self.free_lists[i].find_pre(buddy);
-            if !node.is_null() {
-                self.free_lists[i].pop(node);
-                now = min(now, buddy);
-            } else {
-                unsafe {
-                    self.free_lists[i].push_front(now);
-                }
-                break;
-            }
+            self.block[i] = node_size;
         }
     }
 
     fn calc_size(&self, layout: &Layout) -> usize {
         max(
-            max(layout.align(), self.minimum),
+            max(layout.align(), minimum),
             layout.size().next_power_of_two(),
         )
     }
 
     pub fn alloc(&mut self, layout: Layout) -> *mut u8 {
         let size = self.calc_size(&layout);
-        let level = size.trailing_zeros() as usize;
-        for i in level..self.free_lists.len() {
-            if !self.free_lists[i].is_empty() {
-                self.split(i, level);
-                let res = self.free_lists[level]
-                    .pop_front()
-                    .expect("[buddy allocator] Free list is empty.");
-                self.used += size;
-                self.need += layout.size();
-                return res as *mut u8;
+        assert!(size.is_power_of_two());
+        let mut node_size = self.total;
+        let mut idx = 0;
+        while node_size != size {
+            if idx >= self.block.len() || (idx as isize) < 0 {
+                panic!("[buddy allocator] Index out of bounds.");
             }
+            if self.block[(idx << 1) + 1] >= size {
+                idx = (idx << 1) + 1;
+            } else {
+                idx = (idx << 1) + 2;
+            }
+            node_size >>= 1;
         }
-        null_mut()
+        if self.block[idx] < size {
+            panic!("[buddy allocator] Size not enough. {}, {}, {}, {}", self.block[idx], size, self.used, self.total);
+        };
+        self.used += self.block[idx];
+        self.block[idx] = 0;
+        let offset = (idx + 1) * node_size - self.total;
+        while idx != 0 {
+            idx = ((idx + 1) >> 1) - 1;
+            if idx >= self.block.len() || (idx as isize) < 0 {
+                panic!("[buddy allocator] Index out of bounds.");
+            }
+            self.block[idx] = max(self.block[(idx << 1) + 1], self.block[(idx << 1) + 2]);
+        }
+        (self.offset + offset) as *mut u8
     }
 
     pub fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
         let size = self.calc_size(&layout);
-        let level = size.trailing_zeros() as usize;
-        self.merge(level, ptr);
+        let addr = ptr as usize - self.offset;
+        assert!(addr < self.total);
+        let mut node_size = minimum;
+        let mut idx = addr / minimum + (num >> 1) - 1;
+        assert!(idx < num);
+        while self.block[idx] != 0 {
+            node_size <<= 1;
+            if idx == 0 {
+                return ;
+            }
+            idx = ((idx + 1) >> 1) - 1;
+            if idx >= self.block.len() || (idx as isize) < 0 {
+                panic!("[buddy allocator] Index out of bounds.");
+            }
+        }
+        if self.block[idx] != 0 {
+            panic!("[buddy allocator] No used space")
+        }
+        self.used -= node_size;
+        self.block[idx] = node_size;
+        while idx != 0 {
+            idx = ((idx + 1) >> 1) - 1;
+            if idx >= self.block.len() || (idx as isize) < 0 {
+                panic!("[buddy allocator] Index out of bounds.");
+            }
+            node_size <<= 1;
+            let left = self.block[(idx << 1) + 1];
+            let right = self.block[(idx << 1) + 2];
+            if left + right == node_size {
+                self.block[idx] = node_size;
+            } else {
+                self.block[idx] = max(left, right);
+            }
+        }
     }
 }
--- allocator/src/lib.rs
+++ allocator/src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
 
 pub mod buddy_allocator;
-pub mod linked_list;
 
 extern crate alloc;
